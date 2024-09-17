5bc7aa4435367709a376824217546c317d74a904
Jeroimo <jeronimoriveros@gmail.com>
2024-09-16 23:12:05 -0500

    updating files

--- Backend/main.py
+++ Backend/main.py
@@ -1,11 +1,30 @@
 from fastapi import FastAPI
-from .routers import expenses
-from .db.database import engine
-from .models import Base
+from router.expense import expenseController
+from database.database import engine, Base
+from database.models import Base
+from fastapi.middleware.cors import CORSMiddleware  
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+  "http://localhost:3000",
+  "https://mastervault.vercel.app",
+  "https://mastervault-aaozxopju-max1mus5.vercel.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=["*"],
+)
+
+#creaacion de la BD:
+Base.metadata.create_all(bind=engine)
+
 
 @app.get("/status")
 async def health_check():
