3744a07d5155d3032b47e6c682dc94806872bcd6
Jeroimo <jeronimoriveros@gmail.com>
2024-09-16 23:12:05 -0500

    updating files

---
+++ Backend/database/database.py
@@ -0,0 +1,18 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm.session import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+
+
+sqlite_file_name = "./database.db"
+base_dir = os.path.dirname(os.path.realpath(__file__))
+
+database_url = f"{os.getenv("DATABASE_URL")}+{os.path.join(base_dir, sqlite_file_name)}"
+
+engine = create_engine(database_url,
+                       echo=True)
+
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
